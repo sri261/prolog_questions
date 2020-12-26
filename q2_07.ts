0dec4fab3115c278ae11a9560a5c90cce16626c5
sri261 <dsriharsh@gmail.com>
2020-12-26 16:46:44 +0530

    Greatest common divisor of two positive numbers

--- q2_07.ts
+++ q2_07.ts
@@ -1,4 +1,12 @@
 // 2.07 (**) Determine the greatest common divisor of two positive integer numbers.
+// Use Euclid's algorithm.
+// Example:
+// ?- gcd(36, 63, G).
+// G = 9
+
+// Define gcd as an arithmetic function; so you can use it like this:
+// ?- G is gcd(36,63).
+// G = 9
 
 function greatestCommonDivisor(int1: number, int2: number) {
   while (int1 % int2 > 0) {
