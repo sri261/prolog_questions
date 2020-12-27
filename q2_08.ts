3f6144e9410eae7539e8892287f7500dec1cc44a
sri261 <dsriharsh@gmail.com>
2020-12-27 13:47:10 +0530

    Eulers totient function

--- q2_08.ts
+++ q2_08.ts
@@ -2,7 +2,7 @@
 // Two numbers are coprime if their greatest common divisor equals 1.
 import { greatestCommonDivisor } from "./q2_07";
 
-function checkIfCoprime(int1: number, int2: number) {
-  return greatestCommonDivisor(int1, int2) === 1 ? "Coprime" : "Not Coprime";
+export function checkIfCoprime(int1: number, int2: number) {
+  return greatestCommonDivisor(int1, int2) === 1 ? true : false;
 }
 // console.log(checkIfCoprime(10, 15));
---
+++ q2_09.ts
@@ -0,0 +1,14 @@
+// 2.09 (**) Calculate Euler's totient function phi(m).
+// Euler's so-called totient function phi(m) is defined as the number of positive integers r (1 <= r < m)
+// that are coprime to m.
+
+import { checkIfCoprime } from "./q2_08";
+
+function eulersTotientFn(m: number) {
+  let count: number = 0;
+  for (let i = 0; i <= m; i++) {
+    checkIfCoprime(i, m) ? count++ : null;
+  }
+  return count;
+}
+console.log(eulersTotientFn(20));
