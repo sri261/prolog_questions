144237f0f43fdea84d3e51f8d7974cc48f3bf5a4
sri261 <dsriharsh@gmail.com>
2020-12-28 21:09:13 +0530

    modified to include imported function

--- q1_21.ts
+++ q1_21.ts
@@ -1,13 +1,18 @@
 // 1.21 (*) Insert an element at a given position into a list.
+import { splitList } from "./q1_17";
+import { flatten } from "./q1_07";
+
 const list21: Array<number | string | boolean> = [0, 1, 2, 3, 4, 5, 6, 7];
 
 function insertEleKth(input: Array<any>, kthpositon: number, ele: any) {
   if (kthpositon > input.length) {
-    return "ERROR:Enter Kthpositon smaller that length of list";
+    throw new Error("Enter Kthpositon smaller that length of list");
   } else {
-    return input
-      .slice(0, kthpositon)
-      .concat(ele, input.slice(kthpositon + 1, input.length));
+    const temp: Array<any> = [];
+    temp[0] = splitList(input, kthpositon)[0];
+    temp[1] = ele;
+    temp[2] = splitList(input, kthpositon)[1];
+    return flatten(temp);
   }
 }
-console.log(insertEleKth(list21, 6, false));
+console.log(insertEleKth(list21, 3, false));
