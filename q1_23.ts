d7eee6108cda2f08cc3da5ec7a112ff905b0e98d
sri261 <dsriharsh@gmail.com>
2020-12-28 09:40:15 +0530

    export function added

--- q1_23.ts
+++ q1_23.ts
@@ -1,7 +1,7 @@
 // 1.23 (**) Extract a given number of randomly selected elements from a list.
 const list23: Array<any> = [0, 1, 2, 3, 4, 5, 6, 7];
 
-function randomElements(input: Array<any>, numOfEle: number) {
+export function randomElements(input: Array<any>, numOfEle: number) {
   let newList: Array<any> = [];
   for (let i = 0; i <= numOfEle; i++) {
     newList.push(input[Math.floor(Math.random() * (input.length - 1))]);
@@ -9,4 +9,4 @@ function randomElements(input: Array<any>, numOfEle: number) {
   return newList;
 }
 
-console.log(randomElements(list23, 3));
+// console.log(randomElements(list23, 3));
