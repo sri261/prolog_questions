d8cb436bdedd48fe7986a8e7d09c033b4c3b23ae
sri261 <dsriharsh@gmail.com>
2020-12-29 12:40:37 +0530

    Permutations of an array

--- q1_25.ts
+++ q1_25.ts
@@ -4,19 +4,34 @@
 // L = [b,a,d,c,e,f]
 import { randomElements } from "./q1_23";
 
-const List25: Array<any> = ["a", "b", "c", "d", "e", "f"];
-function randomPermutationOfList(input: Array<any>) {
-  let tempList: Array<number> = [];
-  for (let i = 0; i <= input.length; i++) {
-    let temp = input[Math.floor(Math.random() * (input.length - 1))];
-    if (!tempList.indexOf(temp)) {
-      tempList.push(temp);
-    }
-    // tempList.indexOf(temp) ? tempList.push(temp) : null;
-    return tempList;
-  }
+const List25: Array<any> = ["a", "b", "c"];
+function permutationOfList<T>(input: Array<T>) {
+  const ret = [];
 
-  //   return randomElements(input, input.length);
+  for (let i = 0; i < input.length; i = i + 1) {
+    let rest = permutationOfList(input.slice(0, i).concat(input.slice(i + 1)));
+
+    if (!rest.length) {
+      ret.push([input[i]]);
+    } else {
+      for (let j = 0; j < rest.length; j = j + 1) {
+        ret.push([input[i]].concat(rest[j]));
+      }
+    }
+  }
+  return ret;
 }
 
-console.log(randomPermutationOfList(List25));
+// console.log(perm([1,2,3]).join("\n"));
+console.log(permutationOfList(List25));
+// let tempList: Array<number> = [];
+// for (let i = 0; i <= input.length; i++) {
+//   let temp = input[Math.floor(Math.random() * (input.length - 1))];
+//   if (!tempList.indexOf(temp)) {
+//     tempList.push(temp);
+//   }
+//   // tempList.indexOf(temp) ? tempList.push(temp) : null;
+//   return tempList;
+// }
+
+//   return randomElements(input, input.length);
