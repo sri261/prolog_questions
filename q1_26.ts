e97b4829ea6d334e36d0fd6fb5a879d06c2fb48c
sri261 <dsriharsh@gmail.com>
2020-12-29 13:00:20 +0530

    combinations

--- q1_26.ts
+++ q1_26.ts
@@ -1,20 +1,32 @@
 // 1.26 (**) Generate the combinations of K distinct objects chosen from the N elements of a list
-const List26: Array<any> = ["a", "b", "c", "d", "e", "f", "g"];
-const List264: Array<any> = ["a", "b", "c"];
+const List26: Array<any> = ["a", "b", "c", "d"];
 
-function combinations(input: Array<any>, k) {
-  //[0] [loop 1-last] [loop 2-last]
-  let combiList: Array<any> = [];
-
-  for (let j = 0; j < k; j++) {
-    for (let i = j; i < input.length; i++) {
-      let combi: Array<any> = [];
-      combi.push(input[i]);
-      return combi;
-    }
-    combiList.push();
+function combinations<T>(input: Array<T>, k: number) {
+  if (k > input.length || k <= 0) {
+    return [];
   }
-  return combiList;
+
+  if (k == input.length) {
+    return [input];
+  }
+
+  if (k == 1) {
+    const combs = [];
+    for (let i = 0; i < input.length; i++) {
+      combs.push([input[i]]);
+    }
+    return combs;
+  }
+
+  const combs = [];
+  for (let i = 0; i < input.length - k + 1; i++) {
+    const head = input.slice(i, i + 1);
+    const tailcombs = combinations(input.slice(i + 1), k - 1);
+    for (let j = 0; j < tailcombs.length; j++) {
+      combs.push(head.concat(tailcombs[j]));
+    }
+  }
+  return combs;
 }
 
-console.log(combinations(List264, 2));
+console.log(combinations(List26, 3));
