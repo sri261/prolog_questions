1dd6b71b6d86664f883b7cb2246f4c6a28981e5d
sri261 <dsriharsh@gmail.com>
2020-12-30 09:23:31 +0530

    map method corrected

--- q1_14.ts
+++ q1_14.ts
@@ -2,14 +2,16 @@
 // Example:
 // ?- dupli([a,b,c,c,d],X).
 // X = [a,a,b,b,c,c,c,c,d,d]
+import { flatten } from "./q1_07";
 
 const list14 = ["a", 2, 4, 5, 6, true];
 
 function duplicateElements<T>(input: Array<T>) {
-  let duplicateElementsList: Array<any> = [];
-  input.map((e) => {
-    duplicateElementsList.push(e, e);
-  });
-  return duplicateElementsList;
+  return flatten(
+    input.map((e) => {
+      return [e, e];
+    })
+  );
 }
+
 console.log(duplicateElements(list14));
