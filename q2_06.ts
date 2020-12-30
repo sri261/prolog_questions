b38f6e0ce98c50ae741b0decd495fbd8d9f86b39
sri261 <dsriharsh@gmail.com>
2020-12-30 11:58:39 +0530

    Refactored getEvenNumbers() using filter method

--- q2_06.ts
+++ q2_06.ts
@@ -2,22 +2,20 @@
 //  print a list of all even numbers and their Goldbach composition.
 import { getGoldbachPrimeNumbers } from "./q2_05";
 import { flatten } from "./q1_07";
+import { integerFunction } from "./q1_22";
 
 function checkIfEven(input: number) {
   return input % 2 === 0 ? true : false;
 }
 
 function getEvenNumbers(int1: number, int2: number) {
-  const evenNumbers: Array<number> = [];
-  if (int1 >= int2) {
-    throw new Error("int 1 should be smaller than int 2");
-  } else {
-    for (let i = int1; i <= int2; i++) {
-      checkIfEven(i) ? evenNumbers.push(i) : null;
+  return integerFunction(int1, int2).filter((e) => {
+    if (checkIfEven(e)) {
+      return e;
     }
-  }
-  return evenNumbers;
+  });
 }
+
 function goldbachComposition(int1: number, int2: number) {
   let evenNumList: Array<any> = [getEvenNumbers(int1, int2)];
 
@@ -28,4 +26,4 @@ function goldbachComposition(int1: number, int2: number) {
 
 // console.log(checkIfEven(17));
 // console.log(getEvenNumbers(10, 20));
-console.log(goldbachComposition(20, 25));
+// console.log(goldbachComposition(20, 25));
